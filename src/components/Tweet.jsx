e24fd55ccebf5160cb507ef6c65bad27415b4b23
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-05 11:12:41 +0530

    added edit feature

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import '../CSS/Tweet.css'
 function Tweet({content,likeCount,createdAt}){
+    const [isEditing,setisEditing]=useState(false);
+
    return(
 
     <div className="tweet-wrapper">
-        <div className="tweet-content">
-            {content}
+        <div className="tweet-edit-wrapper">
+            <div className="tweet-content">
+                {(isEditing) ? <input/> : content}
+            </div>
+            <div className="edit-tweet">
+                <button onClick={()=>setisEditing(!isEditing)} >
+                    {(isEditing)?'Save': 'Edit'}
+                </button>
+            </div>
         </div>
+
        <div className="like-createdAt-wrapper">
-         <div className="likes">
-            {likeCount} likes
-        </div>
-        <div className="created-at">
-           <b>Tweeted at </b>  {createdAt}
-        </div>
+          
+            <div className="likes">
+                {likeCount} likes
+            </div>
+            
+            <div className="created-at">
+                <b>Tweeted at </b>  {createdAt}
+            </div>
+           
         </div>
     </ div>
     )
